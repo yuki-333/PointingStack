9da832ec1b835c843fdad4403d3274688e5ca48e
yuki nara <yukinara@NYnoMacBook-Air.local>
2019-08-24 16:31:28 +0900

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,188 +1,171 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
-import { WebBrowser } from 'expo';
+import {
+  createStackNavigator
+} from 'react-navigation';
+import { 
+  moderateScale,
+  scale,
+ } from 'react-native-size-matters';
 
-import { MonoText } from '../components/StyledText';
+import FruitsScreen from './Fruits';
+import VegesScreen from './Veges'
+import SetumeiScreen from './Setsumei'
+import ToolsScreen from './Tools';
+import CarsScreen from './Cars'
 
-export default class HomeScreen extends React.Component {
+const SCREEN_WIDTH = Dimensions.get('window').width
+
+class HomeScreen extends Component {
   static navigationOptions = {
-    header: null,
+    title: 'HOME',
+    headerStyle: {backgroundColor: 'skyblue'},
+    headerTintColor: 'white',
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '絵CARD'
     }
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+  render() {
+    return(
+      <View style={styles.base}>
+      <View style={styles.message}>
+        <Text style={styles.textStyle1}>{this.state.message}</Text>
+      </View>
+        <View style={styles.contents}>
+     
+        <View style={styles.allButtonContainer}>
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+          <TouchableOpacity style={styles.button} onPress={this.doPageAction0}>
+            <Text style={styles.buttonText} >果物</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={this.doPageAction1}>
+          <Text style={styles.buttonText} >野菜</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={this.doPageAction4}>
+          <Text style={styles.buttonText} >乗り物</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={this.doPageAction3}>
+          <Text style={styles.buttonText} >日用品</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={this.doPageAction2}>
+          <Text style={styles.buttonText} >CREDIT</Text>
+          </TouchableOpacity>
+
+        </View>
+        </View>
+          <View style={styles.footer}>
+            <Text style={styles.textStyle2}  >© 2019 ST Curly</Text>
+          </View>
+      </View>
     );
-  };
+  }
+  doPageAction0 = () => {
+    this.props.navigation.navigate('Fruits')
+  }
+  doPageAction1 = () => {
+    this.props.navigation.navigate('Veges')
+  }
+  doPageAction2 = () => {
+    this.props.navigation.navigate('Setumei')
+  }
+  doPageAction3 = () => {
+    this.props.navigation.navigate('Tools')
+  }
+  doPageAction4 = () => {
+    this.props.navigation.navigate('Cars')
+  }
 }
 
+export default createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Fruits: { screen: FruitsScreen },
+    Veges: { screen: VegesScreen },
+    Setumei: { screen: SetumeiScreen },
+    Tools: { screen: ToolsScreen},
+    Cars: {screen: CarsScreen},
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+
 const styles = StyleSheet.create({
-  container: {
+  base: {
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
+    backgroundColor: 'lavender',
     alignItems: 'center',
   },
-  helpLink: {
-    paddingVertical: 15,
+  message: {
+    flex: 2,
+    width: SCREEN_WIDTH - 20,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'royalblue',
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  contents: {
+    flex: 14,
+    width: SCREEN_WIDTH - 20,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  allButtonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row'
+  },
+  button: {
+    flex: 1,
+    margin: scale(10),
+    height: scale(55),
+    width: scale(300),
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'skyblue',
+    borderColor: 'lightskyblue',
+    borderWidth: 5,
+    borderRadius: 30,
+  },
+  buttonText: {
+    fontSize: scale(27),
+    color: 'white',
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'powderblue',
+    width: SCREEN_WIDTH - 20,
+  },
+  textStyle1: {
+    fontSize: scale(30),
+    color: 'white',
+  },
+  textStyle2: {
+    fontSize: moderateScale(10),
+    color: 'black',
+  }
 });
